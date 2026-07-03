3b43cccd479e05067385b03258786c988d413db8
Theekshana <85378827+theekshana623@users.noreply.github.com>
2026-07-03 18:05:20 -0400

    Implement custom hook for MessageBoxW API

--- hooking_engine.cpp
+++ hooking_engine.cpp
@@ -4,25 +4,24 @@
 typedef int (WINAPI* PrototypeMessageBoxW)(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType);
 PrototypeMessageBoxW originalMessageBoxW = nullptr;
 
-// මුල් Bytes 12 සුරැකීමට variables
 BYTE originalBytes[12] = { 0 };
 BYTE jumpInstruction[12] = { 0 };
 
-// අපේ Custom Hook Function එක
+
 int WINAPI HookedMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType) {
     std::wcout << L"[LOG] Intercepted API Call in Memory!\n";
 
     DWORD oldProtect;
     
-    // 1. තාවකාලිකව Unhook කිරීම (මුල් Bytes 12 ආපහු ලිවීම)
+    
     VirtualProtect((LPVOID)originalMessageBoxW, 12, PAGE_EXECUTE_READWRITE, &oldProtect);
     memcpy((void*)originalMessageBoxW, originalBytes, 12);
     VirtualProtect((LPVOID)originalMessageBoxW, 12, oldProtect, &oldProtect);
 
-    // 2. දැන් ආරක්ෂිතව මුල් MessageBoxW එක Call කිරීම (Loop වෙන්නේ නැත)
+    
     int result = originalMessageBoxW(hWnd, L"Intercepted successfully without infinite loops!", L"Secured Window", uType);
 
-    // 3. නැවත Hook එක ස්ථාපනය කිරීම (Re-hook)
+   
     VirtualProtect((LPVOID)originalMessageBoxW, 12, PAGE_EXECUTE_READWRITE, &oldProtect);
     memcpy((void*)originalMessageBoxW, jumpInstruction, 12);
     VirtualProtect((LPVOID)originalMessageBoxW, 12, oldProtect, &oldProtect);
@@ -39,10 +38,10 @@ void InstallHook() {
     DWORD oldProtect;
     VirtualProtect((LPVOID)originalMessageBoxW, 12, PAGE_EXECUTE_READWRITE, &oldProtect);
 
-    // මුල්ම පියවරේදී `MessageBoxW` හි තිබූ සැබෑ Bytes 12 copy කර තබා ගැනීම
+   
     memcpy(originalBytes, (void*)originalMessageBoxW, 12);
 
-    // 64-bit Absolute Jump එක සකස් කිරීම
+    
     jumpInstruction[0] = 0x48; 
     jumpInstruction[1] = 0xB8; // mov rax, address
     UINT_PTR hookAddress = (UINT_PTR)HookedMessageBoxW;
@@ -50,7 +49,7 @@ void InstallHook() {
     jumpInstruction[10] = 0xFF; 
     jumpInstruction[11] = 0xE0; // jmp rax
 
-    // මුල් API එක මතට අපේ Jump එක ලිවීම
+   
     memcpy((void*)originalMessageBoxW, jumpInstruction, 12);
 
     VirtualProtect((LPVOID)originalMessageBoxW, 12, oldProtect, &oldProtect);
